3a8eba4eafcb88bccaf6f1b5b39a20f632129b7f
Selina <crystalicyrose@gmail.com>
2023-06-13 08:22:29 -0700

    Initial Commit

---
+++ confused/confusedApp.swift
@@ -0,0 +1,17 @@
+//
+//  confusedApp.swift
+//  confused
+//
+//  Created by Selina Zhang on 6/13/23.
+//
+
+import SwiftUI
+
+@main
+struct confusedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
